18c8b87eada991e0f92428f0d8871e3dcdda0597
Ian Castillo <ian.abraham.castillo@gmail.com>
2024-05-16 12:44:25 -0600

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Ian Castillo on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
